9e21d2375a34dca558ee97c7096c4ff1bb7988ba
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-02 21:36:53 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -26,11 +26,11 @@ def first_challenge
         value.each do |flavor|
           delete_if {flavor == "strawberry"}
         end
+        
       end
     end
+    contacts
   end
 
   #remember to return your newly altered contacts hash!
-  contacts
-end
-end
+  
