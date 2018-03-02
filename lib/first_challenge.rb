698e021b659ad8f38ef26a8df00cd97873564da3
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-02 21:36:06 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,11 +14,21 @@ def first_challenge
   }
 
   #your code here
-contacts["Freddy Mercury"].each do |key, value|
-  if key == favorite_icecream_flavors
-    puts key
-    binding.pry
-end
+  contacts.each do |person, data|
+    #at this level, "person" is Jon Snow or Freddy and "data" is a hash of key/value pairs
+    #to iterate over the "data" hash, we can use the following line:
+     data.each do |attribute, value|
+      #at this level, "attribute" is describes the key of :name, :email, :favorite_icecream_flavors, or :knows
+      #we need to first check and see if the key is :favorite_icecream_flavors,
+      #if it is, that means the VALUE is an array that we can iterate over to print out each element
+
+      if attribute == :favorite_icecream_flavors
+        value.each do |flavor|
+          delete_if {flavor == "strawberry"}
+        end
+      end
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
